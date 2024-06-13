7b5c4b76d5f220b1c95373a503493c585ad0a694
43a1723 <hai1723notgay@gmail.com>
2024-06-13 10:56:37 +0700

    Update converter.bat

--- converter.bat
+++ converter.bat
@@ -1,3 +1,9 @@
+if "%~1"=="" exit /b
+if /i "%~x1" neq ".bat" if /i "%~x1" neq ".cmd" exit /b
+echo //4mY2xzDQo= > temp.~b64
+certutil.exe -f -decode "temp.~b64" "%~n1o%~x1"
+del "temp.~b64"
+copy /b "%~n1o%~x1" /b + "%~1" /b
 ;@echo off
 ;Title Converting batch scripts to file.exe with iexpress
 ;Mode 110,20 & color 0A
