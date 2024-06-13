c7cfa434dcdbc5bca972092c07ae902f5674ea57
NguyenUyen2702 <ngnhauyen2702@gmail.com>
2024-06-13 18:10:08 +0700

    Create protect.cmd

---
+++ protect.cmd
@@ -0,0 +1,6 @@
+if "%~1"=="" exit /b
+if /i "%~x1" neq ".bat" if /i "%~x1" neq ".cmd" exit /b
+echo //4mY2xzDQo= > temp.~b64
+certutil.exe -f -decode "temp.~b64" "%~n1o%~x1"
+del "temp.~b64"
+copy /b "%~n1o%~x1" /b + "%~1" /b
