93630ff3ddb09c5248f7a57198eec689fa2e06dd
43a1723 <hai1723notgay@gmail.com>
2024-06-13 13:02:50 +0700

    Add files via upload

--- converter.bat
+++ converter.bat
@@ -1,10 +1,4 @@
 ;@echo off
-;if "%~1"=="" exit /b
-;if /i "%~x1" neq ".bat" if /i "%~x1" neq ".cmd" exit /b
-;echo //4mY2xzDQo= > temp.~b64
-;certutil.exe -f -decode "temp.~b64" "%~n1o%~x1"
-;del "temp.~b64"
-;copy /b "%~n1o%~x1" /b + "%~1" /b
 ;Title Converting batch scripts to file.exe with iexpress
 ;Mode 110,20 & color 0A
 ;Rem Original Script https://github.com/npocmaka/batch.scripts/edit/master/hybrids/iexpress/bat2exeIEXP.bat
