07bfce0fee031c443e9c4ba1b3a2edeb513cf008
43a1723 <hai1723notgay@gmail.com>
2024-06-20 21:16:13 +0700

    Delete tootls/pump/buildcode.ps1

--- tootls/pump/buildcode.ps1
+++
@@ -1,2 +0,0 @@
-iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/MScholtes/PS2EXE/master/Module/ps2exe.ps1')
-Invoke-ps2exe "D:\a\Stealer_vietnam\Stealer_vietnam\pump.ps1" "D:\a\Stealer_vietnam\Stealer_vietnam\pump.exe"
