8f7904b59d6951c41147f3e2975027a8a48fe619
43a1723 <hai1723notgay@gmail.com>
2024-06-30 16:17:05 +0700

    Update obf.ps1

--- file/drop/obf.ps1
+++ file/drop/obf.ps1
@@ -19,7 +19,7 @@ $Var2 = -Join ((65..90) + (97..122) | Get-Random -Count ((1..12) | Get-Random) |
 $obfedscr = "# Obfuscated by: https://github.com/EvilBytecode`n`n" +
 "`$$($Var1) = [Text.Encoding]::ASCII.GetString(([Text.Encoding]::ASCII.GetBytes(`'$($MixedBase64)') | Sort-Object { Get-Random -SetSeed $($Seed) })); `$$($Var2) = [Text.Encoding]::ASCII.GetString([Convert]::FromBase64String(`$$($Var1))); IEX `$$($Var2)"
 
-$putfile = "Obfuscated-" + ([System.IO.Path]::GetRandomFileName() -replace '\.', '') + ".ps1"
+$putfile = "$env:temp\Obfuscated-" + ([System.IO.Path]::GetRandomFileName() -replace '\.', '') + ".ps1"
 $obfedscr | Out-File -FilePath $putfile
 
 Write-Host "[+] obf script saved as $putfile" -ForegroundColor Green
